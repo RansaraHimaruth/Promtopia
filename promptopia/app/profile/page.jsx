aa910711dde92ccf73def9bccc5d59a12732130e
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-06-10 19:27:57 +0530

    created the profile page

--- promptopia/app/profile/page.jsx
+++ promptopia/app/profile/page.jsx
@@ -1,43 +1,43 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Profile from '@components/profile'
+import Profile from "@components/profile";
 
 const MyProfile = () => {
+  const { data: session } = useSession();
 
-    const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session.user.id}/posts`);
+      const data = await response.json();
 
-    useEffect(() => {
-      const fetchPosts = async () => {
-        const response = await fetch(`/api/users/${session.user.id}/posts`);
-        const data = await response.json();
+      setPosts(data);
+    };
 
-        setPosts(data);
-      };
+    if (session?.user.id) fetchPosts();
+  }, []);
 
-      if(session?.user.id) fetchPosts();
-    }, []);
+  const handleEdit = () => {};
 
-const handleEdit = () => {}
-
-const handleDelete = async () => {}
+  const handleDelete = async () => {};
 
   return (
     <div>
-      <Profile name="My profile"
-      desc="Welcome to your personalized profile page"
-      data={posts}
-      handleEdit={handleEdit}
+      <Profile
+        name="My"
+        desc="Welcome to your personalized profile page"
+        data={posts}
+        handleEdit={handleEdit}
         handleDelete={handleDelete}
       />
     </div>
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
