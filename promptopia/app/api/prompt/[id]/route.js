78d7e4e3879ada2841b00fc64e3fe8133279756a
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-06-11 00:24:22 +0530

    line

--- promptopia/app/api/prompt/[id]/route.js
+++ promptopia/app/api/prompt/[id]/route.js
@@ -1,8 +1,8 @@
+
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 // GET
-
 
 export const GET = async (request, { params }) => {
   try {
@@ -20,42 +20,41 @@ export const GET = async (request, { params }) => {
   }
 };
 
-
 // Patch
 
 export const PATCH = async (request, { params }) => {
   const { prompt, tag } = await request.json();
 
-    try {
-        await connectToDB();
-    
-        const existingPrompt = await Prompt.findById(params.id);
+  try {
+    await connectToDB();
 
-        if (!existingPrompt) {
-            return new Response("Prompt not found", { status: 404 });
-        }
+    const existingPrompt = await Prompt.findById(params.id);
 
-        existingPrompt.prompt = prompt;
-        existingPrompt.tag = tag;
-
-        await existingPrompt.save();
-    
-        return new Response(JSON.stringify(existingPrompt), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to update prompt", { status: 500 });
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
     }
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
 };
 
 // Delete
 
 export const DELETE = async (request, { params }) => {
-    try {
-        await connectToDB();
-    
-        await Prompt.findByIdAndDelete(params.id);
-    
-        return new Response("Prompt deleted successfully", { status: 200 });
-    } catch (error) {
-        return new Response("Failed to delete prompt", { status: 500 });
-    }
-}
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndDelete(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt", { status: 500 });
+  }
+};
